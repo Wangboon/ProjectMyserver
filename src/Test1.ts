b6d1fcb66fc94c7fb61ad41b185ee1bc8858b7a7
Wangboon <wangboon_b@cmu.ac.th>
2024-10-01 15:02:28 +0700

    test

--- src/Test1.ts
+++ src/Test1.ts
@@ -1,8 +1,47 @@
 import { Utils } from "./Utils";
+import axios from "axios";
 
-if(Utils.add(1,2) === 3)
-{
-    console.log(0)
-}else{
-    console.log(1)
+const unit_test = async () => {
+    //test1
+    if (Utils.add(1, 2) === 3) {
+        console.log(0)
+    } else {
+        console.log('add test1')
+        return;
+    }
+    8
+    //test2
+    if (Utils.add(2, 2) === 4) {
+        console.log(0)
+    } else {
+        console.log('add test2')
+        return;
+    }
+
+    //test addUser
+    const data: any = {
+        "name": "pop",
+        "username": "iampop"
+    }
+
+    try {
+        const response: any = await Utils.addUser(data);
+        const correct_result: any = { "name": "pop", "username": "iampop" }
+
+        const keys: string[] = Object.keys(correct_result);
+        for (const k of keys) {
+            if (correct_result[k] !== response[k]) {
+                console.log("addUser " + k)
+                return
+            }
+        }
+    }
+    catch (error) {
+        console.log("addUser" + error);
+    }
+
+
+
 }
+
+unit_test();
